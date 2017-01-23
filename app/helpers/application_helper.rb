70b5d81a5662f110fac36a54299a50f14376c7e8
vitalypaprotsky <vetalpaprotsky@gmail.com>
2017-01-23 23:21:17 +0200

    change full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,6 +17,6 @@ module ApplicationHelper
 
   def full_title(page_title)
     base_title = 'Repeek'
-    page_title.blank? ? base_title : "#{base_title} | #{page_title}"
+    page_title.blank? ? base_title : "#{page_title} | #{base_title}"
   end
 end
