928480cbe28b017e97d960abe6a378bc6a488fe7
vitalypaprotsky <vetalpaprotsky@gmail.com>
2017-01-28 21:32:18 +0200

    add center_box helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,4 +19,8 @@ module ApplicationHelper
     base_title = 'Repeek'
     page_title.blank? ? base_title : "#{page_title} | #{base_title}"
   end
+
+  def center_box(&block)
+    content_tag(:div, capture(&block), class: 'col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-4 offset-lg-4')
+  end
 end
