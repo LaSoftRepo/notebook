8b3d3a46d89c8fd5980dd780b95a957a73efdcb5
vitalypaprotsky <vetalpaprotsky@gmail.com>
2017-02-11 23:15:16 +0200

    add box helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,7 +20,24 @@ module ApplicationHelper
     page_title.blank? ? base_title : "#{page_title} | #{base_title}"
   end
 
-  def center_box(&block)
+  def center(&block)
     content_tag(:div, capture(&block), class: 'col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-4 offset-lg-4')
+  end
+
+  def box(options = {}, &block)
+    title, button_text, button_link = options[:title], options[:button_text], options[:button_link]
+
+    if title.present?
+      title_tag = content_tag :div, title, class: 'title'
+    end
+
+    if button_text.present?
+      link_tag = link_to(button_text, button_link, class: 'btn btn-primary')
+      button_tag = content_tag :div, link_tag, class: 'button'
+    end
+
+    top_box = content_tag(:div, title_tag + button_tag, id: 'top-box')
+    content_box = content_tag(:div, capture(&block), id: 'content-box')
+    content_tag(:div, (top_box + content_box).html_safe, id: 'box')
   end
 end
