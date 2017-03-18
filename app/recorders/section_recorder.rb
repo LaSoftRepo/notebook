7e630d6a9389ea7ae16d2d0940fce9dafc76d2a1
vitalypaprotsky <vetalpaprotsky@gmail.com>
2017-03-18 19:24:40 +0200

    add exception to SectionRecorder

--- app/recorders/section_recorder.rb
+++ app/recorders/section_recorder.rb
@@ -1,6 +1,7 @@
 class SectionRecorder
 
   class TooDeepEmbedding < StandardError; end
+  class CantFindParentSection < StandardError; end
 
   def initialize(args = {})
     args.reverse_merge!({ section: nil })
@@ -11,6 +12,7 @@ class SectionRecorder
   def create(params, parent_section_id = nil)
     section = if parent_section_id.present?
       parent_section = @notebook.find_section(parent_section_id)
+      raise CantFindParentSection unless parent_section
       raise TooDeepEmbedding unless parent_section.can_create_child_section?
       parent_section.child_sections.create params
     else
