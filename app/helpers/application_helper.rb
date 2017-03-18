ee8a5febe7aa658e05ba5ec660d3b8c2861c5f39
vitalypaprotsky <vetalpaprotsky@gmail.com>
2017-03-18 20:20:42 +0200

    set head title in box helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -25,24 +25,40 @@ module ApplicationHelper
   end
 
   def box(args = {}, &block)
-    # Button is not in use
-    # args.reverse_merge!({ button_link: '#', button_data: {} })
-
-    title = args[:title]
+    # args.reverse_merge!({ button_link: '#', button_data: {}, head_title: true })
+    #
+    # title = args[:title]
     # button_text = args[:button_text]
     # button_link = args[:button_link]
     # button_data = args[:button_data].merge(class: 'btn btn-primary')
-
-    if title.present?
-      title_tag = content_tag :div, title, class: 'title'
-    end
-
+    #
+    # if title.present?
+    #   title_tag = content_tag :div, title, class: 'title'
+    #   # I'm not sure if this is right but it removes a lot of duplications
+    #   provide :head_title, title if args[:head_title]
+    # end
+    #
     # if button_text.present?
     #   link_tag = link_to(button_text, button_link, button_data)
     #   button_tag = content_tag :div, link_tag, class: 'button'
     # end
-
+    #
     # top_box = content_tag(:div, title_tag + button_tag, id: 'top-box')
+    # content_box = content_tag(:div, capture(&block), id: 'content-box')
+    # content_tag(:div, (top_box + content_box).html_safe, id: 'box')
+
+    # Realization without button
+
+    args.reverse_merge!({ head_title: true })
+
+    title = args[:title]
+
+    if title.present?
+      title_tag = content_tag :div, title, class: 'title'
+      # I'm not sure if this is right but it removes a lot of duplications
+      provide :head_title, title if args[:head_title] == true
+    end
+
     top_box = content_tag(:div, title_tag, id: 'top-box')
     content_box = content_tag(:div, capture(&block), id: 'content-box')
     content_tag(:div, (top_box + content_box).html_safe, id: 'box')
