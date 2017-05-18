80a8371113440ebe91e929ff6fdf192d9d8fdd81
vitalypaprotsky <vetalpaprotsky@gmail.com>
2017-05-18 10:18:22 +0300

    add tests for edit, update and find_section methods in sections controller

--- spec/controllers/sections_controller_spec.rb
+++ spec/controllers/sections_controller_spec.rb
@@ -1,19 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe SectionsController, type: :controller do
+  let(:notebook) { FactoryGirl.create(:notebook, user: controller.current_user) }
+  let(:section) { FactoryGirl.create(:section, notebook: notebook) }
+
   describe 'AUTHENTICATION' do
     it "doesn't allow unauthenticated users to access all actions" do
       expect(controller).to filter(:before, with: :authenticate_user)
     end
 
-    it "verifies notebook for all actions" do
+    it 'verifies notebook in all actions' do
       expect(controller).to filter(:before, with: :verify_notebook)
     end
   end
 
   describe 'ACTIONS' do
     log_in
-    let(:notebook) { FactoryGirl.create(:notebook, user: controller.current_user) }
 
     describe 'GET #index' do
       context 'current notebook belongs to current user' do
@@ -96,11 +98,123 @@ RSpec.describe SectionsController, type: :controller do
     end
 
     describe 'GET #edit' do
-      # TODO: SectionsController#edit tests
+      let(:params) { { notebook_id: notebook.id, id: section.id } }
+
+      it 'renders sections/edit template' do
+        get :edit, params: params
+        expect(response).to render_template 'sections/edit'
+      end
     end
 
     describe 'PATCH #update' do
-      # TODO: SectionsController#update tests
+      context 'VALID PARAMS' do
+        let(:valid_params) do
+          {
+            section: FactoryGirl.attributes_for(:section),
+            notebook_id: notebook.id,
+            id: section.id
+          }
+        end
+
+        it 'updates section' do
+          patch :update, params: valid_params
+          section.reload
+          valid_params[:section].each do |key, value|
+            expect(section[key]).to eq value
+          end
+        end
+
+        it 'sets flash[:success] message' do
+          patch :update, params: valid_params
+          expect(flash[:success]).to be_present
+        end
+
+        it 'redirects to notices index action' do
+          patch :update, params: valid_params
+          expect(response).to(
+            redirect_to notebook_section_notices_path(section_id: section.id)
+          )
+        end
+      end
+
+      context 'INVALID PARAMS' do
+        let(:invalid_params) do
+          {
+            section: FactoryGirl.attributes_for(:invalid_section),
+            notebook_id: notebook.id,
+            id: section.id
+          }
+        end
+
+        it 'does not update notebook' do
+          patch :update, params: invalid_params
+          section.reload
+          invalid_params[:section].each do |key, value|
+            expect(section[key]).not_to eq value
+          end
+        end
+
+        it 'sets flash[:error] message' do
+          patch :update, params: invalid_params
+          expect(flash[:error]).to be_present
+        end
+
+        it "renders 'sections/edit' template with status 422" do
+          patch :update, params: invalid_params
+          expect(response).to render_template('sections/edit')
+          expect(response.status).to eq 422
+        end
+      end
+    end
+  end
+
+  describe '#find_section' do
+    it 'is called as before action for edit, update and destroy' do
+      expect(controller).to(
+        filter(:before, with: :find_section, only: [:edit, :update])
+      )
+    end
+
+    context 'LOGGED IN' do
+      log_in
+
+      it 'finds section of current notebook by id from params' do
+        controller.params[:notebook_id] = notebook.id
+        controller.params[:id] = section.id
+        controller.send(:find_section)
+        expect(assigns(:section)).to eq section
+      end
+
+      it 'finds child section of current notebook by id from params' do
+        # TODO Create a few child sections factories with different embedding
+        # TODO and test it here.
+        # TODO Or it's even better to check that find_section method was called
+        # TODO and write tests for that method in notebook_spec.rb file
+      end
+
+      context 'INVALID ID' do
+        it 'raises error ActionController::RoutingError' do
+          controller.params[:notebook_id] = notebook.id
+          controller.params[:id] = FactoryGirl.create(:section).id
+          expect do
+            controller.send(:find_section)
+          end.to raise_error ActionController::RoutingError
+        end
+      end
+    end
+
+    context 'LOGGED OUT' do
+      log_out
+
+      it 'raises error NoMethodError' do
+        notebook = FactoryGirl.create(:notebook)
+        section  = FactoryGirl.create(:section, notebook: notebook)
+        controller.params[:notebook_id] = notebook.id
+        controller.params[:id] = section.id
+        expect do
+          controller.send(:find_section)
+        end.to raise_error NoMethodError
+      end
     end
   end
 end
