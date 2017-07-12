4e505b700fb518e2575f0563b7764ca828936302
Vitaly Paprotsky <vetalpaprotsky@gmail.com>
2017-07-12 19:59:15 +0300

    fix deploy_to_staging.sh

--- .circleci/deploy_to_staging.sh
+++ .circleci/deploy_to_staging.sh
@@ -4,10 +4,19 @@ sh .circleci/install_docker.sh
 sh .circleci/push_to_dockerhub.sh $TAG
 
 echo "Start deploying to staging..."
-PLUGIN_URL=$STAGING_RANCHER_URL
-PLUGIN_ACCESS_KEY=$STAGING_RANCHER_ACCESS_KEY
-PLUGIN_SECRET_KEY=$STAGING_RANCHER_SECRET_KEY
-PLUGIN_SERVICE=repeek/web
-PLUGIN_DOCKER_IMAGE=vitalikpaprotsky/repeek:$TAG
-docker run -v $(pwd):$(pwd) -w $(pwd) peloton/drone-rancher
+# PLUGIN_URL=$STAGING_RANCHER_URL
+# PLUGIN_ACCESS_KEY=$STAGING_RANCHER_ACCESS_KEY
+# PLUGIN_SECRET_KEY=$STAGING_RANCHER_SECRET_KEY
+# PLUGIN_SERVICE=repeek/web
+# PLUGIN_DOCKER_IMAGE=vitalikpaprotsky/repeek:$TAG
+docker run \
+  -e PLUGIN_URL=$STAGING_RANCHER_URL \
+  -e PLUGIN_ACCESS_KEY=$STAGING_RANCHER_ACCESS_KEY \
+  -e PLUGIN_SECRET_KEY=$STAGING_RANCHER_SECRET_KEY \
+  -e PLUGIN_SERVICE=repeek/web \
+  -e PLUGIN_DOCKER_IMAGE=vitalikpaprotsky/repeek:$TAG \
+  -v $(pwd):$(pwd) \
+  -w $(pwd) \
+  peloton/drone-rancher
+# docker run -v $(pwd):$(pwd) -w $(pwd) peloton/drone-rancher
 echo "Deployed to staging"
