b05ea5bf648d09eb61a93dcac3f8f4578b4b1cd4
Vitaly Paprotsky <vetalpaprotsky@gmail.com>
2017-07-12 19:33:16 +0300

    fix push_to_dockerhub.sh

--- .circleci/push_to_dockerhub.sh
+++ .circleci/push_to_dockerhub.sh
@@ -1,3 +1,5 @@
+TAG=$1 # Release tag which is used as docker image tag
+
 echo "Start pushing vitalikpaprotsky/repeek:${TAG} to Dockerhub..."
 docker build -t vitalikpaprotsky/repeek:$TAG .
 docker login -u $DOCKER_USER -p $DOCKER_PASS
