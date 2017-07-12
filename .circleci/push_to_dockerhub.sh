d59bb02b9eca4c1546e27005223743b3f1d3445b
Vitaly Paprotsky <vetalpaprotsky@gmail.com>
2017-07-12 15:19:37 +0300

    add debug echos

--- .circleci/push_to_dockerhub.sh
+++ .circleci/push_to_dockerhub.sh
@@ -1,5 +1,6 @@
+echo "Start pushing vitalikpaprotsky/repeek:${TAG} to Dockerhub"
 TAG=$1
 docker build -t vitalikpaprotsky/repeek:$TAG .
 docker login -u $DOCKER_USER -p $DOCKER_PASS
 docker push vitalikpaprotsky/repeek:$TAG
-echo "Pushed vitalikpaprotsky/repeek:${TAG} tag to Dockerhub"
+echo "Pushed vitalikpaprotsky/repeek:${TAG} to Dockerhub"
