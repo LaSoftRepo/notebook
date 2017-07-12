9aaf6d8fb6ed8ec1c5130d54e36088785e1e4175
Vitaly Paprotsky <vetalpaprotsky@gmail.com>
2017-07-12 16:29:28 +0300

    add minor changes to circle ci sh scripts

--- .circleci/push_to_dockerhub.sh
+++ .circleci/push_to_dockerhub.sh
@@ -1,5 +1,5 @@
-echo "Start pushing vitalikpaprotsky/repeek:${TAG} to Dockerhub"
 TAG=$1
+echo "Start pushing vitalikpaprotsky/repeek:${TAG} to Dockerhub..."
 docker build -t vitalikpaprotsky/repeek:$TAG .
 docker login -u $DOCKER_USER -p $DOCKER_PASS
 docker push vitalikpaprotsky/repeek:$TAG
