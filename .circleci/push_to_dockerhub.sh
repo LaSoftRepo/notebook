3e233c96474ec84f452a427ccd021f9e1427eba8
Vitaly Paprotsky <vetalpaprotsky@gmail.com>
2017-07-13 14:12:17 +0300

    fix .circleci/push_to_dockerhub.sh

--- .circleci/push_to_dockerhub.sh
+++ .circleci/push_to_dockerhub.sh
@@ -3,14 +3,14 @@ TAG=$1 # Release tag which is used as docker image tag
 ENV=$2 # App environment
 IMAGE="vitalikpaprotsky/repeek-${ENV}:${TAG}"
 
-echo "Start pushing vitalikpaprotsky/repeek-${ENV}:${TAG} to Dockerhub..."
+echo "Start pushing ${IMAGE} to Dockerhub..."
 
 docker build -t $IMAGE -f Dockerfile.$ENV . &&
 docker login -u $DOCKER_USER -p "${DOCKER_PASS}qqq" &&
 docker push $IMAGE &&
 SUCESS=1
 
-if [[ $SUCESS == 1 ]]; then
+if [ $SUCESS == 1 ]; then
   echo "Pushed ${IMAGE} to Dockerhub"
 else
   echo "There was an error during pushing ${IMAGE} to Dockerhub"
