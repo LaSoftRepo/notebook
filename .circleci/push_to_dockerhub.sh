236e9b71b6caa60dfa9f0d7a0de282fe054b3061
Vitaly Paprotsky <vetalpaprotsky@gmail.com>
2017-07-13 13:31:14 +0300

    add echos for debug

--- .circleci/push_to_dockerhub.sh
+++ .circleci/push_to_dockerhub.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-set -e
 TAG=$1 # Release tag which is used as docker image tag
 ENV=$2 # App environment
 
 echo "Start pushing vitalikpaprotsky/repeek-${ENV}:${TAG} to Dockerhub..."
 docker build -t vitalikpaprotsky/repeek-$ENV:$TAG -f Dockerfile.$ENV .
+echo "Exit code ${$?}"
 docker login -u $DOCKER_USER -p "${DOCKER_PASS}qqq"
+echo "Exit code ${$?}"
 docker push vitalikpaprotsky/repeek-$ENV:$TAG
 echo "Exit code ${$?}"
 echo "Pushed vitalikpaprotsky/repeek-${ENV}:${TAG} to Dockerhub"
