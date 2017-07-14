f40aa7b2b8b21f6b29fcb560e5aa96a7d2e7c18a
Vitaly Paprotsky <vetalpaprotsky@gmail.com>
2017-07-14 18:03:14 +0300

    testing

--- .circleci/install_docker.sh
+++ .circleci/install_docker.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 echo "Start installing docker..."
-set -x
-VER="17.03.0-ce"
-curl -L -o /tmp/docker-$VER.tgz https://get.docker.com/builds/Linux/x86_64/docker-$VER.tgz
-tar -xz -C /tmp -f /tmp/docker-$VER.tgz
-mv /tmp/docker/* /usr/bin
-echo "Installed docker"
+
+VER="17.03.0-ce" # Docker version
+
+curl -L -o /tmp/dohhhcker-$VER.tgz https://get.docker.com/builds/Linux/x86_64/docker-$VER.tgz &&
+tar -xz -C /tmp -f /tmp/docker-$VER.tgz &&
+mv /tmp/docker/* /usr/bin &&
+SUCESS=1
+
+if [ "$SUCESS" == 1 ]; then
+  echo "Installed docker"
+else
+  echo "There was an error during installing docker"
+  exit 1
+fi
