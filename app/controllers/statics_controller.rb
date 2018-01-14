7b8626508267811a1641794c4bdd561495084175
Vitaly Paprotsky <vetalpaprotsky@gmail.com>
2018-01-14 18:17:06 +0200

    add testing puts

--- app/controllers/statics_controller.rb
+++ app/controllers/statics_controller.rb
@@ -1,5 +1,9 @@
 class StaticsController < ApplicationController
   def home
+    puts "**************************************"
+    puts request.ip
+    puts request.remote_ip
+    puts "**************************************"
     render 'statics/home'
   end
 end
